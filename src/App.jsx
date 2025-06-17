823162d7f9a1133a5af3df56bbeb9abd4142e979
EmirCoban <emircobanoffical@gmail.com>
2025-06-18 01:54:41 +0300

    api key added. movie list.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
@@ -10,12 +10,29 @@ import WatchListButton from "./components/WatchListButton";
 import MovieList from "./components/MovieList";
 import WatchList from "./components/WatchList";
 
-import { movie_list } from "./data";
+const api_key = "b06c4279d23840a7ced8ecb94f0faff4";
+const page = 1;
+const query = "batman";
+const language = "en-US";
 
 export default function App() {
-  const [movies, setMovies] = useState(movie_list);
+  const [movies, setMovies] = useState([]);
   const [watchListMovies, setWatchListMovies] = useState([]);
   const [isWatchListOpen, setIsWatchListOpen] = useState(false);
+
+  // mounting => ilk render edilme anı
+  // re-render => state değiştiğinde yeniden render edilme anı
+  // unmount => componentin DOM'dan kaldırılması
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=batman&page=${page}&language=${language}&query=${query}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  }, []);
 
   function handleAddToWatchList(movie) {
     const isAddedToList = watchListMovies.map((i) => i.id).includes(movie.id);
