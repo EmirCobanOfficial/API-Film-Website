74cc44309ab1cb85e3d40a1958ef6e7a6869a461
EmirCoban <emircobanoffical@gmail.com>
2025-06-28 01:02:36 +0300

    Movie Details code added, fix

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import WatchListButton from "./components/WatchListButton";
 
 import MovieList from "./components/MovieList";
 import WatchList from "./components/WatchList";
+import Movie from "./components/Movie";
+import MovieDetails from "./components/MovieDetails";
 
 const api_key = "b06c4279d23840a7ced8ecb94f0faff4";
 const page = 1;
@@ -24,6 +26,7 @@ export default function App() {
   const [loading, setloading] = useState(false);
   const [error, setError] = useState("");
   const [searchQuery, setSearchQuery] = useState(query);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
     async function getMovies() {
@@ -33,25 +36,25 @@ export default function App() {
           `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&page=${page}&language=${language}&query=${searchQuery}`
         );
 
-        if (!response.status === 404) {
-          throw new Error("Film bulunamadı (Movie not found)");
-        } else if (response.status === 401) {
-          throw new Error("API anahtarı geçersiz (Invalid API key)");
-        } else if (response.status === 500) {
-          throw new Error("Sunucu hatası (Server error)");
-        } else if (response.status === 503) {
-          throw new Error(
-            "Servis geçici olarak kullanılamıyor (Service unavailable)"
-          );
-        } else if (response.status === 429) {
-          throw new Error("Çok fazla istek yapıldı (Too many requests)");
-        } else if (response.status === 400) {
-          throw new Error("Geçersiz istek (Bad request)");
-        } else if (response.status === 403) {
-          throw new Error("Erişim reddedildi (Access denied)");
-        } else if (response.status === 408) {
-          throw new Error("İstek zaman aşımına uğradı (Request timed out)");
-        }
+        // if (!response.status === 404) {
+        //   throw new Error("Film bulunamadı (Movie not found)");
+        // } else if (response.status === 401) {
+        //   throw new Error("API anahtarı geçersiz (Invalid API key)");
+        // } else if (response.status === 500) {
+        //   throw new Error("Sunucu hatası (Server error)");
+        // } else if (response.status === 503) {
+        //   throw new Error(
+        //     "Servis geçici olarak kullanılamıyor (Service unavailable)"
+        //   );
+        // } else if (response.status === 429) {
+        //   throw new Error("Çok fazla istek yapıldı (Too many requests)");
+        // } else if (response.status === 400) {
+        //   throw new Error("Geçersiz istek (Bad request)");
+        // } else if (response.status === 403) {
+        //   throw new Error("Erişim reddedildi (Access denied)");
+        // } else if (response.status === 408) {
+        //   throw new Error("İstek zaman aşımına uğradı (Request timed out)");
+        // }
 
         if (!response.ok) {
           throw new Error("Hata Oluştu (Error)");
@@ -91,6 +94,11 @@ export default function App() {
     setWatchListMovies((movies) => movies.filter((i) => i.id !== movie.id));
   }
 
+  function handleSelectedMovie(movie) {
+    setSelectedMovie(movie);
+    window.scrollTo(0, 0);
+  }
+
   return (
     <>
       <Header>
@@ -103,6 +111,13 @@ export default function App() {
       </Header>
 
       <Main>
+        {selectedMovie && (
+          <MovieDetails
+            movieObj={selectedMovie}
+            onClose={() => setSelectedMovie(null)}
+          />
+        )}
+
         <WatchList
           movies={watchListMovies}
           isWatchListOpen={isWatchListOpen}
@@ -112,7 +127,11 @@ export default function App() {
         {loading && <Loading />}
 
         {!loading && !error && (
-          <MovieList movies={movies} onAddToList={handleAddToWatchList} />
+          <MovieList
+            movies={movies}
+            onAddToList={handleAddToWatchList}
+            onSelectedMovie={handleSelectedMovie}
+          />
         )}
         {error && <ErrorMessage message={error} />}
       </Main>
